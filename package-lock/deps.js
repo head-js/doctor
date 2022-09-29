7ab550d575ead3594a09c004895830f095ea6049
hbrls <shuaizhexu@gmail.com>
2022-09-29 12:04:37 +0800

    F: package-lock defaults vue

--- package-lock/deps.js
+++ package-lock/deps.js
@@ -6,41 +6,52 @@ const PROJECT_PATH = process.cwd();
 const configFile = path.resolve(PROJECT_PATH, '.head-cli.config.js');
 
 
-const byPrefix = ['@types/', 'babel-preset-', 'eslint-plugin-'];
+const byPrefix = [
+  '@babel/preset-', '@babel/plugin-',
+  'eslint-plugin-',
+  // core
+  '@vue/',
+  // ui core
+  '@vant/',
+  // deprecated
+  'babel-preset-', 'babel-plugin-',
+];
 
 
 const byExact = [
   // NOTE: build
-  "@babel/core", "@babel/types", "@babel/preset-env", "@babel/preset-react", "@babel/runtime", "@babel/runtime-corejs3",
-  "@babel/generator", "@babel/plugin-proposal-async-generator-functions", "@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-decorators", "@babel/plugin-proposal-optional-chaining",
-  "babel-loader",
-  "webpack", "webpack-cli", "webpack-merge", "terser-webpack-plugin", "url-loader", "vue-loader",
-  "assets-webpack-plugin", "css-loader", "cssnano", "mini-css-extract-plugin", "optimize-css-assets-webpack-plugin",
-  "postcss", "postcss-calc", "postcss-import", "postcss-loader", "postcss-ordered-values", "postcss-simple-vars", "postcss-url",
-  "gulp", "del",
-  // NOTE: build < 2
-  "babel-runtime", "babel-core", "babel-polyfill", "babel-plugin-syntax-jsx", "babel-plugin-transform-decorators", "babel-plugin-transform-regenerator", "babel-plugin-transform-runtime", "babel-plugin-transform-vue-jsx",
-  "gulp-replace", "gulp-util",
-  "autoprefixer", "json-loader", "uglify-js",
+  '@babel/core', '@babel/plugin-transform-runtime', '@babel/runtime', '@babel/template', '@babel/generator', '@babel/preset-env', '@babel/preset-env', '@babel/types',
+  'core-js', 'browserslist',
+  'babel-loader', 'webpack', 'webpack-cli', 'webpack-merge', 'terser-webpack-plugin', 'url-loader',
+  'vue-loader', 'vue-template-compiler',
+  'assets-webpack-plugin', 'css-loader', 'cssnano', 'mini-css-extract-plugin', 'optimize-css-assets-webpack-plugin',
+  'postcss', 'postcss-calc', 'postcss-import', 'postcss-loader', 'postcss-ordered-values', 'postcss-simple-vars', 'postcss-url',
+  'autoprefixer',
+  'gulp', 'json-loader', 'uglify-js',
   // NOTE: dev
-  "style-loader",
-  "babel-eslint", "eslint", "eslint-config-airbnb", "eslint-config-airbnb-base", "eslint-import-resolver-node", "eslint-import-resolver-webpack", "eslint-loader",
-  "stylelint", "stylelint-config-standard", "stylelint-config-standard", "stylelint-webpack-plugin",
-  "webpack-bundle-analyzer", "webpack-dev-middleware", "webpack-dev-server",
+  'style-loader',
+  'babel-eslint', 'eslint', 'eslint-config-airbnb', 'eslint-config-airbnb-base', 'eslint-import-resolver-node', 'eslint-import-resolver-webpack', "eslint-loader",
+  'stylelint', 'stylelint-config-standard', 'stylelint-config-standard', 'stylelint-webpack-plugin',
+  'webpack-bundle-analyzer', 'webpack-dev-middleware', 'webpack-dev-server',
+  'vconsole', 'vconsole-webpack-plugin',
   // NOTE: core
-  "ajv", "axios", "core-js", "dayjs", "react", "react-dom", "prop-types", "react-redux", "react-router", "react-router-redux", "redux", "redux-saga", "immutable", "path-to-regexp",
-  "vue", "vue-router", "vuex",
-  "lodash", "lodash-es", "vanilla.js",
+  'ajv', 'axios', 'dayjs', 'react', 'react-dom', 'prop-types', 'react-redux', 'react-router', 'react-router-redux', 'redux', 'redux-saga', 'immutable',
+  'path-to-regexp',
+  'vue', 'vue-router', 'vuex',
+  'lodash', 'lodash-es', 'vanilla.js',
   // NOTE: ui core
-  "antd", "rc-form", "rc-menu", "rc-table", "rc-tabs", "react-contextmenu",
-  "dva-core", "dva-react-router-3", "dva-loading", "antd-dayjs-webpack-plugin",
-  "draft-js", "dayjs",
-  "@vant/*",
+  'antd', 'rc-form', 'rc-menu', 'rc-table', 'rc-tabs', 'react-contextmenu',
+  'dva-core', 'dva-react-router-3', 'dva-loading', 'antd-dayjs-webpack-plugin',
+  'draft-js', 'dayjs',
+  'vant',
   // NOTE: deprecated
-  "gulp-replace", "classnames", "happypack", "moment", "postcss-scss", "run-sequence", "uglifyjs-webpack-plugin",
+  'babel-runtime', 'babel-core', 'babel-polyfill',
+  '@babel/runtime-corejs3',
+  'gulp-replace', 'classnames', 'happypack', 'moment', 'run-sequence', 'uglifyjs-webpack-plugin',
   // NOTE: security risk
-  "minimatch", "minimist", "graceful-fs",
+  // 'minimatch', 'minimist', 'graceful-fs', 'chokidar'
 ];
+
 
 if (fs.existsSync(configFile)) {
   const config = require(configFile)['package-lock'] || {};
